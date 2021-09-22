958fb53e50d33af98de0bb268a31237dff560fd8
aldiharlian24 <aldiharlian24@gmail.com>
2021-09-22 13:00:14 +0700

    tambah codeigniter

---
+++ webprog2/app/Controllers/Aldi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Aldi extends BaseController
+{
+    public function index()
+    {
+        return view('aldi');
+    }
+}
---
+++ webprog2/app/Controllers/latihan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class latihan extends BaseController
+{
+    public function index()
+    {
+        return view('latihan/index');
+    }
+}
